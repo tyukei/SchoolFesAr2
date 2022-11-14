3da0bb7286edfdf601c0255054325b7c815607a0
Keita Nakata <0024481294bxyk6037@gmail.com>
2022-11-14 20:48:37 +0900

    updae: trans scene

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public string[] sceneNames;
     // Start is called before the first frame update
-    void Start()
+    public void TransScene(int index)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(sceneNames[index]);
     }
 }
